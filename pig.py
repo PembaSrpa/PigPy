96559c7862a6ff93ed2beb156ff83c9be1d1e95c
PembaSrpa <nirashanichang@gmail.com>
2025-12-07 09:09:02 +0545

    pigg

--- pig.py
+++ pig.py
@@ -1,8 +1,3 @@
-# This project implements a simple text-based version of the classic dice game "Pig."
-# Players take turns rolling a die, accumulating points each turn unless they roll a 1,
-# which causes them to lose their points for that round. The first player to reach the
-# target score wins. The game supports 2 to 4 players.
-
 import random
 
 def roll():
